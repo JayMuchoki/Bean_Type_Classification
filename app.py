27ae3a5daab2c7a30da06eae99b82fdbc4a4a273
JayMuchoki <mainajacob324@gmail.com>
2025-07-18 13:30:09 +0300

    changes file from the archives

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ st.write('This model Predict Dr Beans types according to the input features belo
 
 Perimeter=st.slider('Perimeter:',538,1847)
 Eccentricity=st.slider('Eccentricity:',0.4,0.9)
-Solidity=st.slider('Solidity:',0.96,0.99)
+Solidity=st.slider('Solidity:',0.90,0.99)
 roundness=st.slider('roundness:',0.68,0.99)
-ShapeFactor1=st.slider('ShapeFactor1:',0.00,0.01)
-ShapeFactor2=st.slider('ShapeFactor2:',0.00,0.01)
-ShapeFactor4=st.slider('ShapeFactor4:',0.98,1.00)
+ShapeFactor1=st.slider('ShapeFactor1:',0.00,0.10)
+ShapeFactor2=st.slider('ShapeFactor2:',0.00,0.10)
+ShapeFactor4=st.slider('ShapeFactor4:',0.90,1.00)
 
 features=np.array([[Perimeter,Eccentricity,Solidity,roundness,ShapeFactor1,ShapeFactor2,ShapeFactor4]])
 scaled_features=scaler.transform(features)
